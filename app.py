8f2d29e7e3a742bc3f6c012650ed366d44a014f2
ps253993 <ps253993@summacollege.nl>
2026-04-14 12:26:56 +0200

    login gekoppeld aan db
    Login en sign-up gekoppeld aan database

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, session, url_for, redirect, request, render_template
 import secrets
+import sqlite3
+import hashlib
+
+db = sqlite3.connect("verbruikData.db", check_same_thread=False)
 
 app = Flask(__name__)
 
 app.secret_key = secrets.token_hex()
 
-#test username en password
-user = "test"
-password = "Password01"
+def check_user(username, password):
+    #check of username en password in database staan
+    pswrd = hashlib.sha256(password.encode()).hexdigest()
+    user = db.execute("SELECT * FROM users WHERE user_name = ? AND user_password = ?", (username, pswrd)).fetchone()
+
+    if user:
+        return True
+    else:
+        return False
 
 #index
 @app.route("/")
@@ -22,13 +32,28 @@ def index():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+
         #check voor juiste login gegevens
-        if request.form['username'] == user and request.form["password"] == password: 
+        if check_user(request.form['username'], request.form['password']) and request.form['btn'] == "login": 
             #voeg username aan sessie toe
             session['username'] = request.form['username']
             return redirect(url_for("index"))
-        else:
-            return render_template("login.html")
+        elif request.form['username'] != "" and request.form['password'] != "" and request.form['btn'] == "signup":
+            #check of username al in database staat
+            user = db.execute("SELECT user_name FROM users WHERE user_name = ?", (request.form['username'],)).fetchone()
+        
+            if user == None:
+                #hash wachtwoord
+                pswrd = hashlib.sha256(request.form['password'].encode()).hexdigest()
+                # Voeg nieuwe gebruiker toe aan database
+                db.execute("INSERT INTO users (user_name, user_password) VALUES (?, ?)", (request.form['username'], pswrd))
+                db.commit()
+                #zeg dat account is aangemaakt
+                return render_template("login.html", status="Account aangemaakt!")
+            else:
+                #anders zeg dat account al bestaat
+                return render_template("login.html", status="Username al in gebruik!")
+            
     #weergeef inlog pagina    
     return render_template("login.html")
 
